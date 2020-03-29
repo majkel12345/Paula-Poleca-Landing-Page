33cfe5b3d878d373fe3546d0548b7b547a0b1a0a
Andrzej Stasiński <andis_as@wp.pl>
2020-03-29 19:52:17 +0200

    Menu NAV

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var nav = document.querySelector("#nav");
+var menu = document.querySelector("#nav .icon");
+menu.onclick = function() {
+    var flag = nav.classList.toggle('responsive');
+}
+
+var option = document.querySelectorAll('.nav a:not(.icon)');
+option.forEach(function(item){
+    item.onclick = function() {
+        if(nav.classList.contains('responsive'))
+            nav.classList.toggle('responsive');
+    }
+});
+
+
+
