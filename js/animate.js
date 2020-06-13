26518a06da601e9944eff40e9f21888d0f5a0ff4
Volodymyr Hannibal <v.m.hannibal@gmail.com>
2020-06-13 15:05:07 +0200

    Add translate, add folder css, rename folder img

--- js/animate.js
+++ js/animate.js
@@ -1,3 +1,32 @@
+// bounceInDown Logo
+
+$('.logo')
+    .waypoint( function(dir) {
+        if ( dir === 'down' )
+            $(this)
+            .addClass('bounceInDown')
+            .removeClass('fadeOutDown');
+        else
+            $(this)
+            .addClass('fadeOutDown')
+            .removeClass('bounceInDown');
+        }, {
+            offset: '80%'
+        })
+    .waypoint( function(dir) {
+        if ( dir === 'down' )
+            $(this)
+            .addClass('fadeOutDown')
+            .removeClass('bounceInDown');
+        else
+            $(this)
+            .addClass('bounceInDown')
+            .removeClass('addHidden')
+            .removeClass('fadeOutDown');
+         }, {
+             offset: -95
+         })   
+         
 //zoomInDown O nas
 
 $('.onas__info')
@@ -23,63 +52,5 @@ $('.onas__info')
             $(this)
             .addClass('.onas__info');
     }, {
-        offset: -50
+        offset: -95
     })
-
-// bounceInDown Logo
-
-$('.logo')
-         .waypoint( function(dir) {
-             if ( dir === 'down' )
-                 $(this)
-                 .addClass('bounceInDown')
-                 .removeClass('fadeOutDown');
-             else
-                 $(this)
-                 .addClass('fadeOutDown')
-                 .removeClass('bounceInDown');
-         }, {
-             offset: '80%'
-         })
-          .waypoint( function(dir) {
-             if ( dir === 'down' )
-                 $(this)
-                 .addClass('fadeOutDown')
-                 .removeClass('bounceInDown');
-             else
-                 $(this)
-                 .addClass('bounceInDown')
-                 .removeClass('addHidden')
-                 .removeClass('fadeOutDown');
-         }, {
-             offset: -50
-         })   
-         
-//fadeInUp
-
-/*$('.onas__info, .logo')
-         .waypoint( function(dir) {
-             if ( dir === 'down' )
-                 $(this)
-                 .removeClass('fadeOutDown')
-                 .addClass('fadeInUp');
-             else
-                 $(this)
-                 .removeClass('fadeInUp')
-                 .addClass('fadeOutDown');
-         }, {
-             offset: '90%'
-         })
-          .waypoint( function(dir) {
-             if ( dir === 'down' )
-                 $(this)
-                 .removeClass('fadeInUp')
-                 .addClass('fadeOutDown');
-             else
-                 $(this)
-                 .removeClass('fadeOutDown')
-                 .addClass('fadeInUp');
-         }, {
-             offset: -50
-         })
-*/
